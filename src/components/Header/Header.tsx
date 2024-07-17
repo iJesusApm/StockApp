c92bbb315a40b1c60e4cc35e418f5a8be5acce83
iJesusApm <jesusapm14@gmail.com>
2024-07-17 11:03:10 -0400

    feat: :zap: small change in header to handle call to actions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TouchableOpacity} from 'react-native'
 
 import {useTranslation} from 'react-i18next'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import {useTheme} from '@/theme'
-import layout from '@/theme/layout'
 
-const Header = () => {
-  const {gutters, fonts} = useTheme()
+type Props = {
+  onActionPress?: () => void
+  iconName?: string
+}
+
+const Header = ({onActionPress, iconName = 'circle'}: Props) => {
+  const {gutters, fonts, colors, layout} = useTheme()
   const {t} = useTranslation(['common'])
 
   return (
-    <View style={[layout.row, layout.justifyBetween]}>
-      <Text style={[fonts.gray400, fonts.bold, fonts.size_24, gutters.marginLeft_16, gutters.marginTop_12]}>
-        {t('common:appName.full')}
-      </Text>
+    <View style={[layout.row, layout.justifyBetween, gutters.marginTop_12, gutters.marginHorizontal_12]}>
+      <Text style={[fonts.gray400, fonts.bold, fonts.size_24]}>{t('common:appName.full')}</Text>
+      {Boolean(onActionPress) && (
+        <TouchableOpacity testID="call-to-action-button" style={[layout.justifyCenter]} onPress={onActionPress}>
+          <Icon name={iconName} size={28} color={colors.purple500} />
+        </TouchableOpacity>
+      )}
     </View>
   )
 }
