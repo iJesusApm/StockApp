25ade0191566c7335f2406cbf2c59be9dd0c6e5c
iJesusApm <jesusapm14@gmail.com>
2024-07-17 10:30:38 -0400

    feat: :art: customize tabbar button with icons and colors

--- src/navigators/Application.tsx
+++ src/navigators/Application.tsx
@@ -1,8 +1,9 @@
 import {createStackNavigator} from '@react-navigation/stack'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
 import {NavigationContainer} from '@react-navigation/native'
+
 import {SafeAreaProvider} from 'react-native-safe-area-context'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 import {Example, Startup, Alert, Graph, WatchList} from '@/screens'
 import {useTheme} from '@/theme'
@@ -24,10 +25,48 @@ const Stack = createStackNavigator<RootStackParamList>()
 const Tab = createBottomTabNavigator<RootTabParamList>()
 
 function TabNavigator() {
-  const {variant} = useTheme()
+  const {variant, colors} = useTheme()
 
   return (
-    <Tab.Navigator key={variant} screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      key={variant}
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({color, size}) => {
+          let iconName = ''
+
+          switch (route.name) {
+            case WATCHLIST:
+              iconName = 'star-outline'
+              break
+            case GRAPH:
+              iconName = 'show-chart'
+              break
+            case ALERT:
+              iconName = 'notifications-none'
+              break
+            default:
+              iconName = 'circle'
+              break
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />
+        },
+        tabBarActiveTintColor: colors.purple500,
+        tabBarInactiveTintColor: colors.gray400,
+        tabBarLabel: (() => {
+          switch (route.name) {
+            case WATCHLIST:
+              return 'WatchList'
+            case GRAPH:
+              return 'Graph'
+            case ALERT:
+              return 'Alert'
+            default:
+              return route.name
+          }
+        })(),
+      })}>
       <Tab.Screen name={WATCHLIST} component={WatchList} />
       <Tab.Screen name={GRAPH} component={Graph} />
       <Tab.Screen name={ALERT} component={Alert} />
